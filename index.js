60e5c9be96cff49aa5020fafb534166253231c88
Serzhul <wilshere917@gmail.com>
2021-12-11 14:12:54 +0900

    #9 Error raised again

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('hello');
-console.error('무언가... 잘못된 것 같습니다.');
-console.log('안녕하세요 조나현입니다.');
-console.log('치킨먹고싶다.');
+console.error(new Error('무언가... 잘못된 것 같습니다.'));
+console.log('안녕하세요 조나현입니다. 탐정이죠.');
+console.log('치킨먹고싶다. 추천좀 해주세요.');
