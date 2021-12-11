7ca7e85067a8c439337b446dc64b22740f6d62f1
Serzhul <wilshere917@gmail.com>
2021-12-11 14:10:53 +0900

    #9 Error raised

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 console.log('hello');
-console.log('안녕하세요 서대원입니다.');
-console.log("안녕하세요 조나현입니다.")
-console.log("치킨먹고싶다.");
+console.error('무언가... 잘못된 것 같습니다.');
+console.log('안녕하세요 조나현입니다.');
+console.log('치킨먹고싶다.');
