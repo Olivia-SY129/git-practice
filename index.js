a02c75ce739aa787e7aed7cb783235616d968b1d
Seungyeon Lee <sie9412@gmail.com>
2021-12-11 14:25:22 +0900

    added

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-console.log('hello');
-console.log('안녕하세요 서대원입니다.');
-console.log("안녕하세요 조나현입니다.")
+console.log("hello");
+console.log("안녕하세요 서대원입니다.");
+console.log("안녕하세요 조나현입니다.");
+console.log("안녕하세요 이승연입니다.");
 console.log("치킨먹고싶다.");
